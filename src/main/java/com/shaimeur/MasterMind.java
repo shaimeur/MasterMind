e3e41520c4641f1a898b23cf47ff59e5dc41647d
shaimeur <haimeursaad@gmail.com>
2021-12-02 02:25:42 +0100

    modification sans ArrayList

--- src/main/java/com/shaimeur/MasterMind.java
+++ src/main/java/com/shaimeur/MasterMind.java
@@ -1,21 +1,69 @@
 package com.shaimeur;
 
-import java.util.Random;
+import java.util.*;
 
 public class MasterMind {
+
+    private static final char OK = '#';
+    private static final char FAUX = 'O';
+
     public static void main(String[] args) {
-        int n = 4;
-        int m = 6;
-        int[] laCombinaison = new  int[4];
-        int[] combinaison = new  int[6];
 
-        int count = tirerCombinaison();
+        Scanner  scanner = new Scanner(System.in);
+
+        int taille = 4;
+        int max = 6;
+        int maxRound = 10 ;
+        int[] solution = new int[taille];
+        
+        Random random = new Random();
+
+        for (int i = 0; i <taille ; i++) {
+            solution[i] = random.nextInt(max+1);
+        }
+
+        int count = 0 ;
+
+        System.out.println("Le codeBraker doit trouver la combinaison" + taille + "  chiffres entre 1 et " + max + '.');
+        System.out.println(OK + "signifie que c'est le bon chiffre à la bonne place et " + FAUX + " signifie que cest le mauvais chiffre " );
+        System.out.println("Attention vous avez droit a " + maxRound + " essais");
+        System.out.println("------------------------------------------------------------------------------------------------------------");
+
+        int[] chiffres = new int[taille];
+        boolean victory = false;
+
+        do {
+            System.out.println("\nEssai n°" + (count + 1) + "/" + maxRound + " :"  );
+            int nombreSaisi = scanner.nextInt();
+                // scan guesses
+            for (int i = 0; i < taille; i++) {
+                chiffres[i] = (int) (nombreSaisi / (Math.pow(10,taille -1))) % 10;
+            }
+            //player guess
+            for (int i = 0; i <taille ; i++) {
+                System.out.println( chiffres[i]+ " ");
+                System.out.println(); // retour à la ligne
+            }
+
+            victory = true ;
+            for (int i = 0; i <taille ; i++) {
+                boolean jackpot = chiffres[i] == solution[i];
+                System.out.println((jackpot ? OK : FAUX) + " ");
+                victory = victory && jackpot;
+            }
+            System.out.println();
+            count++;
+            if (count == maxRound){
+                System.out.println("vous avez épuiser vos " + maxRound + " essai ,vous avez perdu !! :(");
+            }
+
+
+
+        }while (!victory && count < maxRound);{
+            System.out.println("En seulement " + count + " coups");
+            scanner.close();
+        }
 
     }
-    static int tirerCombinaison(int[] uneCombinaison,int m, int n){
-        Random rand = new Random();
 
-        return  rand.nextInt(m - n + 1);
-
-    }
 }
